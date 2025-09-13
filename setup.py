fe44ca3bbb23760e33fb96c8e50cc2a34aac89fc
ZeroxDev <zeroxxdev@gmail.com>
2025-09-13 21:10:53 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Neko",
+    version="0.1.0",
+    description="Easy way to communicate with AI",
+    author="ZeroxDev",
+    packages=find_packages(),
+    author_email="zeroxxdev@gmail.com",
+    py_modules=["openai"]
+)
